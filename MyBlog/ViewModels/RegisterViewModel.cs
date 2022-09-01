ee2cae640ac6f409ca93033f38925e4a591c29ad
Ковылин Алексей <alkovylin@ya.ru>
2022-09-01 20:52:39 +0300

    0109_2

--- MyBlog/ViewModels/RegisterViewModel.cs
+++ MyBlog/ViewModels/RegisterViewModel.cs
@@ -5,12 +5,13 @@ namespace MyBlog.ViewModels
     public class RegisterViewModel
     {
         [Required(ErrorMessage = "Не указан Email")]
+        [EmailAddress(ErrorMessage = "Неверный формат. Пример: name@example.com")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Введите пароль")]
+        [Required(ErrorMessage = "Введите пароль")]        
         [DataType(DataType.Password)]
-        [StringLength(20, ErrorMessage = "Поле {0} должно иметь не менее {2} и не более {1} символов.", MinimumLength = 5)]        
+        [StringLength(20, ErrorMessage = "Поле должно иметь не менее {2} и не более {1} символов.", MinimumLength = 5)]        
         public string RegPassword { get; set; }
 
         [Required(ErrorMessage = "Подтвердите пароль")]
